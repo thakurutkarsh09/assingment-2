c7e60428da1f3a688910ceeeb3629bc7b2cd2be8
thakurutkarsh09 <45357862+thakurutkarsh09@users.noreply.github.com>
2020-03-31 15:44:30 +0000

    Create mymain.java

---
+++ src1/problem2/main/mymain.java
@@ -0,0 +1,19 @@
+ public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(10);
+        obj.insert(1);
+        obj.insert(6);
+        obj.insert(50);
+        obj.insert(40);
+        obj.insert(30);
+        obj.insert(85);
+        obj.insert(63);
+        obj.insert(54);
+        obj.insert(2);
+        obj.insert(23);
+        obj.insert(95);
+        obj.insert(36);
+        prepost();
+    }
+}
